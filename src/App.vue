8d07056e2224f4c02c56f86bad36ba9597470e28
Giotreglia <giovanni.treglia@gmail.com>
2023-03-29 14:53:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import { store } from './store'
+
+export default {
+  components: {
+
+  },
+
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    getCards() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?&fname=Magician')
+        .then(response => {
+          console.log(response.data);
+        })
+    }
+  },
+  created() {
+    this.getCards();
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss"></style>
